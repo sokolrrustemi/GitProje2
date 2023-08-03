f786e06b69b940197ecd6dc9d8a0fbd68cb690b4
PC <edikokici9@hotmail.com>
2023-08-03 18:19:20 +0200

    Musteri istek paneli testi

---
+++ src/Gun01/Ogrenci.java
@@ -0,0 +1,6 @@
+package Gun01;
+
+public class Ogrenci {
+    String ad;
+    String soyad;
+}
---
+++ src/Gun01/Okul.java
@@ -0,0 +1,7 @@
+package Gun01;
+
+public class Okul {
+    public static void main(String[] args) {
+        Ogrenci ogr1=new Ogrenci();
+    }
+}
